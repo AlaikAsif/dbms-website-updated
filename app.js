d9c8ff36ac8e85558797fc2731fd128e6a77d7c0
Mridul Mahajan <114482322+MridulMahaj@users.noreply.github.com>
2024-01-12 08:49:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,88 +4,97 @@
 //Languages - Node.js
 //Database Stored in - MongoDb
 
-const { MongoClient } = require('mongodb');
-const { v4: uuidv4 } = require('uuid');
+//Changes - Separate Collections for all the end users
+const { MongoClient, ObjectId } = require('mongodb');
 const faker = require('faker');
 
 const uriConnection = 'mongodb://localhost:27017';
 const client = new MongoClient(uriConnection);
 
-const studentFieldOfStudy = [
-  'Biochemistry',
-  'Biophysics',
-  'Paediatrics',
-  'Forensic Medicine',
-  'Immunohematology and Blood Transfusion',
-  'Nuclear Medicine',
-  'Microbiology',
-  'Pathology',
-  'Pharmacology',
-  'Anatomy'
-];
+async function insertStudents(collection) {
+  const students = [];
+  for (let i = 0; i < 1000; i++) {
+    students.push({
+      _id: new ObjectId(), // Using ObjectId as a unique identifier
+      name: faker.name.findName(),
+      age: faker.datatype.number({ min: 16, max: 25 }),
+      gender: faker.random.arrayElement(['Male', 'Female']),
+      field_of_study: faker.random.arrayElement([
+        'Biochemistry',
+        'Biophysics',
+        'Paediatrics',
+        'Forensic Medicine',
+        'Immunohematology and Blood Transfusion',
+        'Nuclear Medicine',
+        'Microbiology',
+        'Pathology',
+        'Pharmacology',
+        'Anatomy',
+      ]),
+      qualifications: faker.random.arrayElement([
+        '10th Pass',
+        '12th Pass',
+        '1st Year',
+        '2nd Year',
+        '3rd Year',
+        'Graduate',
+        'Postgraduate',
+      ]),
+    });
+  }
+  await collection.insertMany(students);
+}
 
-const studentQualifications = [
-  '10th Pass',
-  '12th Pass',
-  '1st Year',
-  '2nd Year',
-  '3rd Year',
-  'Graduate',
-  'Postgraduate'
-];
+async function insertDoctors(collection) {
+  const doctors = [];
+  for (let i = 0; i < 1000; i++) {
+    doctors.push({
+      _id: new ObjectId(),
+      name: `Dr. ${faker.name.findName()}`,
+      field_of_study: faker.random.arrayElement([
+        'Neurosurgeon',
+        'Heart Specialist',
+        'Cardiology',
+        'Psychiatry',
+        'Physician',
+        'Nephrology',
+      ]),
+      age: faker.datatype.number({ min: 28, max: 60 }),
+      gender: faker.random.arrayElement(['Male', 'Female']),
+    });
+  }
+  await collection.insertMany(doctors);
+}
 
-const doctorFieldOfStudy = [
-  'Neuro surgeon',
-  'Heart Specialist',
-  'Cardiology',
-  'Psychiatrics',
-  'Physician',
-  'Nephrology'
-];
+async function insertPatients(collection) {
+  const patients = [];
+  for (let i = 0; i < 1000; i++) {
+    patients.push({
+      _id: new ObjectId(),
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      age: faker.datatype.number({ min: 1, max: 100 }),
+      gender: faker.random.arrayElement(['Male', 'Female']),
+      case_description: faker.lorem.words(3),
+    });
+  }
+  await collection.insertMany(patients);
+}
 
 async function insertRecords() {
   try {
     await client.connect();
-    const database = client.db('DBMS');
-    const collection = database.collection('DBMS Project');
+    const database = client.db('HospitalManagementSystem');
+    const studentsCollection = database.collection('Students');
+    const doctorsCollection = database.collection('Doctors');
+    const patientsCollection = database.collection('Patients');
 
-    const records = [];
-    for (let i = 0; i < 1000; i++) {
-      const student = {
-        Student_Name: faker.name.findName(),
-        Student_Age: faker.datatype.number({ min: 16, max: 25 }),
-        Student_Gender: faker.random.arrayElement(['Male', 'Female']),
-        Student_Field_of_Study: faker.random.arrayElement(studentFieldOfStudy),
-        Student_Email: faker.internet.email(),
-        Student_Password: uuidv4(),
-        Student_Qualifications: faker.random.arrayElement(studentQualifications),
-        Student_Phone_Number: faker.phone.phoneNumber()
-      };
+    await Promise.all([
+      insertStudents(studentsCollection),
+      insertDoctors(doctorsCollection),
+      insertPatients(patientsCollection),
+    ]);
 
-      const doctor = {
-        Doctor_Name: `Dr. ${faker.name.findName()}`,
-        Doctor_FieldOfStudy: faker.random.arrayElement(doctorFieldOfStudy),
-        Doctor_Age: faker.datatype.number({ min: 28, max: 60 }),
-        Doctor_Gender: faker.random.arrayElement(['Male', 'Female']),
-        Doctor_Email: faker.internet.email(),
-        Doctor_Password: uuidv4(),
-        Doctor_Phone_Number: faker.phone.phoneNumber()
-      };
-
-      const patient = {
-        Patient_Name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-        Patient_Age: faker.datatype.number({ min: 1, max: 100 }),
-        Patient_Gender: faker.random.arrayElement(['Male', 'Female']),
-        Patient_Case: faker.lorem.words(3),
-        Patient_Email: faker.internet.email(),
-        Patient_PhoneNumber: faker.phone.phoneNumber()
-      };
-
-      records.push({ Student: student, Doctor: doctor, Patient: patient });
-    }
-
-    await collection.insertMany(records);
-    console.log('Sample records inserted successfully.');
+    console.log('Records inserted successfully.');
   } finally {
     await client.close();
   }
